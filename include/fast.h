13ac6de684ad0157b9a9c86a9b7ad8e4bea5c5b5
SiZiOUS <sizious@gmail.com>
2023-05-08 22:15:14 +0200

    Give proper credits to the authors.

--- include/fast.h
+++ include/fast.h
@@ -12,6 +12,11 @@ __BEGIN_DECLS
  *
  * This function converts the argument c into an unsigned char and fills the
  * first n characters of the array pointed to by s to the value.
+ *
+ * Copyright (C) 1999  Niibe Yutaka
+ * Copyright (c) 2009  STMicroelectronics Ltd
+ *   Optimised using 64bit data transfer (via FPU) and the movca.l inst.
+ *   Author: Giuseppe Cavallaro <peppe.cavallaro@st.com>
  */
 void *memset_fast(void *s, int c, size_t n);
 
@@ -23,6 +28,14 @@ void *memset_fast(void *s, int c, size_t n);
  *
  * It is assumed that there is no overlap between src and dst.
  * If there is an overlap, then the results are undefined.
+ *
+ * Copyright (C) 1999  Niibe Yutaka
+ * Copyright (c) 2002  STMicroelectronics Ltd
+ *   Modified from memcpy.S and micro-optimised for SH4
+ *   Stuart Menefy (stuart.menefy@st.com)
+ * Copyright (c) 2009  STMicroelectronics Ltd
+ *   Optimised using prefetching and 64bit data transfer via FPU
+ *   Author: Giuseppe Cavallaro <peppe.cavallaro@st.com>
  */
 void *memcpy_fast(void *dst, const void *src, size_t n);
 
